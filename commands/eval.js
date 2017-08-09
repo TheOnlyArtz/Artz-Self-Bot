7fe35e2fe65ed53f8f3a79891b6e9bec142d41ba
TheOnlyArtz <gamesil456@gmail.com>
2017-08-09 11:41:35 +0300

    Indent fixing

--- commands/eval.js
+++ commands/eval.js
@@ -1,31 +1,30 @@
 module.exports.run = async (client, message, args) => {
-  message.delete();
-  var code = message.content.split(" ").slice(1).join(" ")
-   try {
-      let evaled= eval(code);
-       if (evaled && evaled.constructor.name == 'Promise')
-         evaled = await evaled;
-       if (typeof evaled !== 'string')
-         evaled = require('util').inspect(evaled);
-       message.channel.send(`\nInput📥: \`\`\`xl\n${code}\`\`\`\nOutput📤\n\`\`\`\`xl\n${clean(client, evaled)}\n\`\`\``).catch(console.error)
-   }
-   catch(err) {
-       message.channel.send(`__ERROR__\nInput📥: \`\`\`xl\n${code}\`\`\`\nOutput📤\n\`\`\`xl\n\`\`\`${clean(client, err)}\n\`\`\``);
-   }
- };
+	message.delete();
+	const code = message.content.split(' ').slice(1).join(' ');
+	try {
+		let evaled = eval(code);
+		if (evaled && evaled.constructor.name == 'Promise') {
+			evaled = await evaled;
+		}
+		if (typeof evaled !== 'string') {
+			evaled = require('util').inspect(evaled);
+		}
+		message.channel.send(`\nInput📥: \`\`\`xl\n${code}\`\`\`\nOutput📤\n\`\`\`\`xl\n${clean(client, evaled)}\n\`\`\``).catch(console.error);
+	} catch (err) {
+		message.channel.send(`__ERROR__\nInput📥: \`\`\`xl\n${code}\`\`\`\nOutput📤\n\`\`\`xl\n\`\`\`${clean(client, err)}\n\`\`\``);
+	}
+};
 
- function clean(client, text) {
-   if (typeof(text) === "string") {
-     return text.replace(/`/g, "`" + String.fromCharCode(8203))
-       .replace(/@/g, "@" + String.fromCharCode(8203))
-       .replace(client.token, "You're-really-thinking-that-I'm-that-DUMB?");
-   }
-   else {
-       return text;
-   }
+function clean(client, text) {
+	if (typeof (text) === 'string') {
+		return text.replace(/`/g, '`' + String.fromCharCode(8203))
+       .replace(/@/g, '@' + String.fromCharCode(8203))
+       .replace(client.token, 'You\'re-really-thinking-that-I\'m-that-DUMB?');
+	}
+	return text;
 }
 module.exports.help = {
-  category: 'misc',
-  name: "eval",
-  description: 'Runs a snippet of code'
-}
+	category: 'misc',
+	name: 'eval',
+	description: 'Runs a snippet of code'
+};
--- commands/google.js
+++ commands/google.js
@@ -1,28 +1,32 @@
-const Discord = require("discord.js")
-const google = require("google")
-module.exports.run = async (client, message) => {
+const Discord = require('discord.js');
+const google = require('google');
 
-google.resultsPerPage = 3
-let args= message.content.split(" ").slice(1).join(" ")
-google(args, function (err, res){
-  if (err) logger.error(err)
-  var link = res.links;
-  if (link[0] === undefined) return message.reply("Didnt find")
-  const embed = new Discord.RichEmbed()
-  .setAuthor(`Google search for ${args}`, "http://supple.image.newsru.com/images/big/101_33_1013381_1441378803.jpg")
-  .addField("This what I found", `**${link[0].title}:** ${link[0].href}`)
-  .addField("More results", `**${link[1].title}:** ${link[1].href}\n**${link[2].title}:** ${link[2].href}`)
-  message.channel.send({
-    embed
-  })
+module.exports.run = async (client, message) => {
+	google.resultsPerPage = 3;
+	const args = message.content.split(' ').slice(1).join(' ');
+	google(args, (err, res) => {
+		if (err) {
+			logger.error(err);
+		}
+		const link = res.links;
+		if (link[0] === undefined) {
+			return message.reply('Didnt find');
+		}
+		const embed = new Discord.RichEmbed()
+  .setAuthor(`Google search for ${args}`, 'http://supple.image.newsru.com/images/big/101_33_1013381_1441378803.jpg')
+  .addField('This what I found', `**${link[0].title}:** ${link[0].href}`)
+  .addField('More results', `**${link[1].title}:** ${link[1].href}\n**${link[2].title}:** ${link[2].href}`);
+		message.channel.send({
+			embed
+		})
   .catch(e => {
-    logger.error(e)
-  })
-  });
-}
+	logger.error(e);
+});
+	});
+};
 
 module.exports.help = {
-  category: 'misc',
-  name: "google",
-  description: 'Will give you the ability to look for things on the internet'
-}
+	category: 'misc',
+	name: 'google',
+	description: 'Will give you the ability to look for things on the internet'
+};
