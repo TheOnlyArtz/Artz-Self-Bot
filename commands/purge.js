bd95fd5085ec74696cadcf6078a9dfab362ad607
TheOnlyArtz <gamesil456@gmail.com>
2017-08-09 11:41:46 +0300

    Indent fixing

--- commands/purge.js
+++ commands/purge.js
@@ -1,22 +1,26 @@
 module.exports.run = async (client, message, args) => {
-const user = (message.mentions.users.first() || message.client.users.get(args[0]) || null);
-const amount = !!user ? parseInt(message.content.split(" ")[2], 10) : parseInt(message.content.split(" ")[1], 10);
-if (!amount) return message.edit("Must specify an amount to delete!").then(message.delete(2000));
-if (!amount && !user) return message.edit("Must specify a user and amount, or just an amount, of messages to purge!").then(message.delete(2000));
-await message.delete();
-let messages = await message.channel.fetchMessages({limit: 100});
-if(user) {
-  messages = messages.array().filter(m=>m.author.id === user.id);
-  messages.length = amount;
-} else {
-  messages = messages.array();
-  messages.length = amount + 1;
-}
-messages.map(async m => await m.delete().catch(console.error));
+	const user = (message.mentions.users.first() || message.client.users.get(args[0]) || null);
+	const amount = user ? parseInt(message.content.split(' ')[2], 10) : parseInt(message.content.split(' ')[1], 10);
+	if (!amount) {
+		return message.edit('Must specify an amount to delete!').then(message.delete(2000));
+	}
+	if (!amount && !user) {
+		return message.edit('Must specify a user and amount, or just an amount, of messages to purge!').then(message.delete(2000));
+	}
+	await message.delete();
+	let messages = await message.channel.fetchMessages({limit: 100});
+	if (user) {
+		messages = messages.array().filter(m => m.author.id === user.id);
+		messages.length = amount;
+	} else {
+		messages = messages.array();
+		messages.length = amount + 1;
+	}
+	messages.map(async m => await m.delete().catch(console.error));
 };
 
 module.exports.help = {
-  category: 'fun',
-  name: "purge",
-  description: 'Purge will delete specific amount of messages by someone'
-}
+	category: 'fun',
+	name: 'purge',
+	description: 'Purge will delete specific amount of messages by someone'
+};
--- commands/smoke.js
+++ commands/smoke.js
@@ -1,35 +1,33 @@
 exports.run = (client, message, args) => {
-
-  message.channel.send("**BISSSSHES IM SMOKING**").then(async (msg) => {
-    setTimeout(() => {
-    msg.edit('🚬');
-    }, 500);
-    setTimeout(() => {
-        msg.edit('🚬 ☁ ');
-    }, 1000);
-    setTimeout(() => {
-        msg.edit('🚬 ☁☁ ');
-    }, 1500);
-    setTimeout(() => {
-        msg.edit('🚬 ☁☁☁ ');
-    }, 2000);
-    setTimeout(() => {
-        msg.edit('🚬 ☁☁');
-    }, 2500);
-    setTimeout(() => {
-        msg.edit('🚬 ☁');
-    }, 3000);
-    setTimeout(() => {
-        msg.edit('🚬 ');
-    }, 3500);
-    setTimeout(() => {
-        msg.edit(`Finished smoking`);
-    }, 4000);
-  })
-
+	message.channel.send('**BISSSSHES IM SMOKING**').then(async msg => {
+		setTimeout(() => {
+			msg.edit('🚬');
+		}, 500);
+		setTimeout(() => {
+			msg.edit('🚬 ☁ ');
+		}, 1000);
+		setTimeout(() => {
+			msg.edit('🚬 ☁☁ ');
+		}, 1500);
+		setTimeout(() => {
+			msg.edit('🚬 ☁☁☁ ');
+		}, 2000);
+		setTimeout(() => {
+			msg.edit('🚬 ☁☁');
+		}, 2500);
+		setTimeout(() => {
+			msg.edit('🚬 ☁');
+		}, 3000);
+		setTimeout(() => {
+			msg.edit('🚬 ');
+		}, 3500);
+		setTimeout(() => {
+			msg.edit(`Finished smoking`);
+		}, 4000);
+	});
 };
 module.exports.help = {
-  category : 'fun',
-  name: "smoke",
-  description: 'Smoke weed everyday :dab:'
-}
+	category: 'fun',
+	name: 'smoke',
+	description: 'Smoke weed everyday :dab:'
+};
